3bf916b85f01ea92601d20c2cc2c62a3dc3b1bf1
Mirsha América Mugica <163442366+MirshaMM@users.noreply.github.com>
2024-06-13 00:26:24 -0600

    actu 13/06

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,118 +18,204 @@ namespace figurasgeometricas
             InitializeComponent();
         }
 
-        private void radioButton4_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton7_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void guardar_Click(object sender, EventArgs e)//este boton muestra una ventana al usuario para que sepa que los datos que ingreso se guardaron
         {
-            {
-                if (cuadrado.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de cuadrado");
-                }
-                if (triangulo.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de triangulo");
-                }
-                if (rectangulo.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de rectangulo");
-                }
-                if (polireg.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de pligono regular");
-                }
-                if (poliirreg.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de poligono irregular");
-                }
-                if (circulo.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de circulo");
-                }
-                if (cubo.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de cubo");
-                }
-                if (prisma.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de prisma");
-                }
-                if (esfera.Checked)
-                {
-                    MessageBox.Show("Se guardaron los datos de esfera");
-                }              
-            }
+            MessageBox.Show("DATOS GUARDOS" + GUARDAR.Text, "Datos de alumno",
+       MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void limpiar_Click(object sender, EventArgs e)
         {
-            textBox1.Clear();
+            texto.Clear();
+            imagen.Image = (figurasgeometricas.Properties.Resources.cuadrado);
+            medida.Clear();
             cuadrado.Checked = false;
             triangulo.Checked = false;
             rectangulo.Checked = false;
-            polireg.Checked = false;
-            poliirreg.Checked = false;
+            esfera.Checked = false;
             circulo.Checked = false;
             cubo.Checked = false;
             prisma.Checked = false;
-            esfera.Checked = false;
+            polii.Checked = false;
+            polir.Checked = false;
+            if (MessageBox.Show("¿Deseas salir de la aplicación?", "Aviso",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                this.Close();
+            }
         }
 
-        private void dibujar_Click(object sender, EventArgs e)
+         private void INFORMACION_Click(object sender, EventArgs e)
         {
             if (cuadrado.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\cuadrado.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada: Cuadrado, " + "\n" +
+                    "Sus datos son:" + "\n" +
+                    "Longitud del lado: "
+                    + medida.Text;
             }
-            if (triangulo.Checked)
+            else if (triangulo.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\triangulo.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Triangulo, " +
+                    "Sus datos son:"
+                    + medida.Text;
             }
-            if (circulo.Checked)
+            else if (rectangulo.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\circulo.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Rectangulo, " +
+                    "Sus datos son:"
+                    + medida.Text;
             }
-            if (cubo.Checked)
+            else if (polir.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\cubo.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Poligono Regular, " +
+                    "Sus datos son:"
+                    + medida.Text;
             }
-            if (esfera.Checked)
+            else if (polii.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\esfera.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Poligono Irregular, " +
+                    "Sus datos son:"
+                    + medida.Text;
             }
-            if (poliirreg.Checked)
+            else if (circulo.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\poligono irreg.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Círculo, " +
+                    "Sus datos son:"
+                    + medida.Text;
             }
-            if (polireg.Checked)
+            else if (cubo.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\poligono.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Cubo, " +
+                    "Sus datos son:"
+                    + medida.Text;
             }
-            if (prisma.Checked)
+            else if (prisma.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\poligono.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Prisma, " +
+                    "Sus datos son:"
+                    + medida.Text;
             }
-            if (rectangulo.Checked)
+            else if (esfera.Checked)
             {
-                string rutaImagen = "E:\\Usurios\\Desktop\\figurasgeometricasactualizado-master\\figurasgeometricasactualizado-master\\figuras\\rectangulo.png";
-                imagen.BackgroundImage = Image.FromFile(rutaImagen);
+                texto.Text = "Figura seleccionada Esfera, " +
+                    "Sus datos son:"
+                    + medida.Text;
+            }
+        }
+
+        private void EXPORTAR_Click(object sender, EventArgs e)
+        {
+            try //EXCEPCIÓN
+            {
+                string fileName =
+                    @"E:\Usurios\Desktop\figurasgeometricasactualizado-master\figurasgeometricasactualizado-master\datosg\info.txt";
+                // esto inserta texto en un archivo existente, si el archivo no existe lo crea
+                StreamWriter writer = File.AppendText(fileName);
+                //  writer.WriteLine("Este es un dato nuevo desde guardar");
+                writer.WriteLine("Datos: " + texto.Text);
+                writer.WriteLine("\n");
+                writer.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error al guardar los Datos", "ERROR",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (MessageBox.Show("¿Quieres salir de la aplicacion?", "Aviso",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void CALCULAR_Click(object sender, EventArgs e)
+        {
+            if (cuadrado.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= " + "\n" + "Volumen, no se calcula en figuras planas "
+                    + "\n";
+            }
+            else if (triangulo.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= " + "\n" + "Volumen, no se calcula en figuras planas "
+                   + "\n";
+            }
+            else if (rectangulo.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= " + "\n" + "Volumen, no se calcula en figuras planas "
+                      + "\n";
+            }
+            else if (polir.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= " + "\n" + "Volumen, no se calcula en figuras planas "
+                    + "\n";
+            }
+            else if (polii.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= " + "\n" + "Volumen, no se calcula en figuras planas "
+                   + "\n";
+            }
+            else if (circulo.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= " + "\n" + "Volumen, no se calcula en figuras planas "
+                   + "\n";
+            }
+            else if (cubo.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= no definido" + "\n" + "Volumen= "
+                   + "\n";
+            }
+            else if (prisma.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= no definido" + "\n" + "Volumen= "
+                  + "\n";
+            }
+            else if (esfera.Checked)
+            {
+                texto.Text = texto.Text + "\n" + "Area= " + "\n" + "Perimetro= no definido" + "\n" + "Volumen= "
+                   + "\n";
+            }
+        }
+
+        private void DIBUJAR_Click(object sender, EventArgs e)
+        {
+            if (cuadrado.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.cuadrado);
+            }
+            else if (triangulo.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.triangulo);
+            }
+            else if (rectangulo.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.rectangulo);
+            }
+            else if (polir.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.poligono);
+            }
+            else if (polii.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.poligonoirreg);
+            }
+            else if (circulo.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.circulo);
+            }
+            else if (cubo.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.cubo);
+            }
+            else if (prisma.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.prisma);
+            }
+            else if (esfera.Checked)
+            {
+                imagen.Image = (figurasgeometricas.Properties.Resources.esfera);
             }
         }
     }
